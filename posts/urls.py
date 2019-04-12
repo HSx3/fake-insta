16672cf00c4296f9efc9a4a879b63c8d9aa9a67a
HSx3 <hs.ssafy@gmail.com>
2019-04-12 02:49:22 +0000

    02_CRUD & Image Upload

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from . import views
 
 app_name = 'posts'
 
 urlpatterns = [
-    
+    path('<int:post_pk>/delete/', views.delete, name='delete'),
+    path('<int:post_pk>/edit/', views.update, name='update'),
+    # path('<int:post_pk>/', views.detail, name='detail'),
+    path('create/', views.create, name='create'),
+    path('', views.list, name='list'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_list_or_404, get_object_or_404
+from .models import Post
+from .forms import PostForm
 
 # Create your views here.
+def list(request):
+    posts = get_list_or_404(Post.objects.order_by('-pk'))
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/list.html', context)
+    
+def create(request):
+    # 새 글 등록, 수정 동작
+    if request.method == 'POST':
+        post_form = PostForm(request.POST, request.FILES)
+        if post_form.is_valid():
+            post_form.save()
+            return redirect('posts:list')
+    
+    # 새 글 작성페이지
+    else:
+        post_form = PostForm()
+    context = {
+        'post_form': post_form,
+    }
+    return render(request, 'posts/form.html', context)
+
+# def detail(request, post_pk):
+#     post = get_object_or_404(Post, pk=post_pk)
+#     context = {
+#         'post': post,
+#     }
+#     return render(request, 'posts/detail.html', context)
+
+def update(request, post_pk):
+    post = get_object_or_404(Post, pk=post_pk)  # 수정할 글을 가져오기 위해
+    if request.method == 'POST':
+        post_form = PostForm(request.POST, instance=post)
+        if post_form.is_valid():
+            post_form.save()
+            return redirect('posts:list')
+            # return redirect('posts:detail', post.pk)
+    else:
+        post_form = PostForm(instance=post)     # 수정할 데이터를 가져와야함
+    context = {
+        'post_form': post_form,
+        'post': post,
+    }
+    return render(request, 'posts/form.html', context)
+    
+def delete(request, post_pk):
+    post = get_object_or_404(Post, pk=post_pk)
+    if request.method == 'POST':
+        post.delete()
+    return redirect('posts:list')
+    # else:
+    #     return redirect('posts:detail', post.pk)
