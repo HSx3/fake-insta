f3683eff700c5b67b924ab03ec3e1d383f1c3164
HSx3 <hs.ssafy@gmail.com>
2019-04-17 05:21:14 +0000

    09_model 1:1 relationship & make profile

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,15 @@
 from django.contrib.auth.forms import UserChangeForm
 from django.contrib.auth import get_user_model
+from django import forms
+from .models import Profile
 
 class UserCustomChangeForm(UserChangeForm):
     class Meta:
         model = get_user_model()
         fields = ['email', 'first_name', 'last_name',]
+        
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['nickname', 'introduction',]
+        
