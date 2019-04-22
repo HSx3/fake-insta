55fa4dd202f4cff93f4e45ff6974893ccb889b2d
HSx3 <hs.ssafy@gmail.com>
2019-04-22 07:34:13 +0000

    11_make hashtag

---
+++ c
@@ -0,0 +1,65 @@
+[33mcommit 92c2f0a97889466a184a95882adef7b2c0d39446[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;31morigin/master[m[33m)[m
+Author: HSx3 <hs.ssafy@gmail.com>
+Date:   Wed Apr 17 08:06:40 2019 +0000
+
+    10_make follow
+
+[33mcommit f3683eff700c5b67b924ab03ec3e1d383f1c3164[m
+Author: HSx3 <hs.ssafy@gmail.com>
+Date:   Wed Apr 17 05:21:14 2019 +0000
+
+    09_model 1:1 relationship & make profile
+
+[33mcommit b2fbd5387263e44e8f705a2c7fda419a789c31b1[m
+Author: HSx3 <hs.ssafy@gmail.com>
+Date:   Wed Apr 17 04:12:14 2019 +0000
+
+    08_Auth update delete & password update
+
+[33mcommit 0e2ae1cbbf2c633fda8ccf64cca716ad39d146f2[m
+Author: HSx3 <hs.ssafy@gmail.com>
+Date:   Tue Apr 16 07:58:01 2019 +0000
+
+    07_Like M:N & make people page
+
+[33mcommit 989d8a8c4cd3294c0368e78484016d9617d4c06b[m
+Author: HSx3 <hs.ssafy@gmail.com>
+Date:   Mon Apr 15 08:37:53 2019 +0000
+
+    06_make Comment
+
+[33mcommit 8a1d4cab3abac8fea3b25f5a7bb16754453da7a9[m
+Author: HSx3 <hs.ssafy@gmail.com>
+Date:   Mon Apr 15 06:20:47 2019 +0000
+
+    05_Authentication & 1:N model relateion
+
+[33mcommit 2031218d393e58764c84afd3c1f487cbe6d99567[m
+Author: HSx3 <hs.ssafy@gmail.com>
+Date:   Fri Apr 12 06:25:03 2019 +0000
+
+    04_Upload Multiple Images
+
+[33mcommit 78d0efe2797dd8715a91616518265f8302a32bb3[m
+Author: HSx3 <hs.ssafy@gmail.com>
+Date:   Fri Apr 12 05:05:11 2019 +0000
+
+    03_make favicon
+
+[33mcommit 16672cf00c4296f9efc9a4a879b63c8d9aa9a67a[m
+Author: HSx3 <hs.ssafy@gmail.com>
+Date:   Fri Apr 12 02:49:22 2019 +0000
+
+    02_CRUD & Image Upload
+
+[33mcommit 2b6cc3955aebe28064c3cf34ae5146e7aa671b2f[m
+Author: HSx3 <hs.ssafy@gmail.com>
+Date:   Thu Apr 11 05:35:36 2019 +0000
+
+    01_make post Model & admin setting
+
+[33mcommit c42ee65945d486d614bd05495956cdfd9d48c9d0[m
+Author: HSx3 <hs.ssafy@gmail.com>
+Date:   Thu Apr 11 05:15:59 2019 +0000
+
+    00_Start project && Setting
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_list_or_404, get_object_or_404
-from .models import Post, Image, Comment
+from .models import Post, Image, Comment, Hashtag
 from .forms import PostForm, ImageForm, CommentForm
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
@@ -40,6 +40,32 @@ def create(request):
             post = post_form.save(commit=False)
             post.user = request.user
             post.save()
+            # hashtag - post.save()가 된 이후에 hashtag 코드가 와야함
+            # 1. 게시글을 순회하면서 띄어쓰기를 잘라야 함
+            # 2. 자른 단어가 #으로 시작하나?
+            # 3. 이 해시태그가 기존 해시태그에 있는건지?
+            
+            for word in post.content.split():
+                if word[0] == '#':
+                # if word.startswith('#'):
+                    hashtag = Hashtag.objects.get_or_create(content=word)
+                    post.hashtags.add(hashtag[0])
+                    # (모델객체의 인스턴스, BOOLEAN)을 리턴 
+                    # ex) 없으면 새로 생성 : (hashtag, True), 있으면 : (hashtag, False)
+                    
+                    # hashtag, new = Hashtag.objects.get_or_create(content=word)
+                    # if new:
+                    #     post.hashtags.add(hashtag)
+                    
+            # contents = post.content.split()
+            # for content in contents:
+            #     if content[0] == '#':
+            #         hashtag = Hashtag.objects.create(content=content)
+            #     post.hashtags.add(hashtag)
+            
+            
+            
+            
             for image in request.FILES.getlist('file'):
                 request.FILES['file'] = image
                 image_form = ImageForm(files=request.FILES)
@@ -76,7 +102,16 @@ def update(request, post_pk):
     if request.method == 'POST':
         post_form = PostForm(request.POST, instance=post)
         if post_form.is_valid():
-            post_form.save()
+            # post_form.save()
+            post = post_form.save()
+            # hashtag update
+            post.hashtags.clear()
+            for word in post.content.split():
+                if word[0] == '#':
+                    hashtag = Hashtag.objects.get_or_create(content=word)
+                    post.hashtags.add(hashtag[0])
+            
+            
             return redirect('posts:list')
             # return redirect('posts:detail', post.pk)
     else:
@@ -148,3 +183,13 @@ def explore(request):
         'comment_form': comment_form,
     }
     return render(request, 'posts/explore.html', context)
+    
+    
+def hashtag(request, hash_pk):
+    hashtag = get_object_or_404(Hashtag, pk=hash_pk)
+    posts = hashtag.post_set.order_by('-pk')
+    context = {
+        'hashtag': hashtag,
+        'posts': posts,
+    }
+    return render(request, 'posts/hashtag.html', context)
